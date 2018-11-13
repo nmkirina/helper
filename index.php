3115686d63646132202e52501a46ca6e6f392d66
Irina <sokolovaie@altarix.ru>
2018-11-13 18:55:12 +0300

    test

--- index.php
+++ index.php
@@ -1,72 +1,63 @@
 <?php
 include 'view/main.php';
- $test = Excel::import('/home/irina/projects/rzd-ss/output.csv');
+       $test = Excel::import('/home/irina/projects/rzd-ss/output.csv');
         $prod = Excel::import('/home/irina/projects/rzd-ss/prod.xlsx');
         
-        foreach ($prod as $item){
-            $result[$item['indexname']] = [
-                'schema' => $item['schemaname'],
-                'table' =>  $item['tablename'],
-                'prod' => 1,
-                'test' => 0,
+        foreach ($test as $item)
+        {
+            preg_match("/\((.*)\)/", $item['indexdef'], $matches);
+            if (empty($matches)) {
+                $matches = '';
+            } else{
+                $matches = $matches[1];
+            }
+            $keyField = str_replace(', ', '.', $matches);
+            $fields = explode(',', $matches);
+            $key = $item['schemaname'] . '.' . $item['tablename'] . '.' . $keyField;
+            $result[$key] = [
+               'schema' => $item['schemaname'],
+               'table' => $item['tablename'],
+               'field' => $fields,
+               'indexnameTest' => $item['indexname'],
+               'indexDefTest' => $item['indexdef'],            
             ];
         }
-        foreach ($test as $item){
-            if (key_exists($item['indexname'], $result)) {
-                $result[$item['indexname']]['test'] = 1;
-            } else {
-                $result[$item['indexname']] = [
-                    'schema' => $item['schemaname'],
-                    'table' =>  $item['tablename'],
-                    'prod' => 0,
-                    'test' => 1,
-                ];
-            }
-        }
-        $schemas = [];
-        $onlyProd = [];
-        $onlyTest = [];
-        foreach ($result as $index => $item) {
-            if ($item['prod'] && !$item['test']) {
-                
-                $schemas[$item['schema'] . '.' .$item['table']] = [
-                    'where' => 'prod',
-                    'prod' => $index,
-                    'test' => null,
-                    ];
-                $onlyProd[$index] = [
-                    'schemaname' =>  $item['schema'],
-                    'tablename' => $item['table'],
-                    'indexname' => $index,
-                ];
-                
-            } elseif (!$item['prod'] && $item['test']) {
-                $onlyTest[$index] = $item;
-                $key = $item['schema'] . '.' .$item['table'];
-                if (key_exists($key, $schemas)) {
-                    $schemas[$key]['where'] = 'prod&test';
-                    $schemas[$key]['test'] = $index;
-                    unset($onlyProd[$index]);
-                } else {
-                    $schemas[$key] = [
-                    'where' => 'test',
-                    'test' => $index,
-                    'prod' => null,
-                    ];                     
-                    $onlyTest[$index] = [
-                        'schemaname' =>  $item['schema'],
-                        'tablename' => $item['table'],
-                        'indexname' => $index,
-                    ];
-                }
-            }
-        }
-        foreach ($onlyProd as $item) {
-            $result .= '$this->createIndex({$item["indexname"]}, {$item["schemaname"].$item["tablename"]}, $column);\n';
-        }
+//        foreach ($prod as $item)
+//        {
+//            preg_match("/\((.*)\)/", $item['indexdef'], $matches);
+//             if (empty($matches)) {
+//                $matches = '';
+//            } else{
+//                $matches = $matches[1];
+//            }
+//            $keyField = str_replace(', ', '.', $matches);
+//            $fields = explode(',', $matches);
+//            $key = $item['schemaname'] . '.' . $item['tablename'] . '.' . $keyField;
+//           
+//            if (!key_exists($key, $result)){
+//                $result[$key]['schema'] = $item['schemaname'];
+//                $result[$key]['table'] = $item['tablename'];
+//                $result[$key]['field'] = $fields;
+//                $result[$key]['indexnameProd'] = $item['indexname'];
+//                $result[$key]['indexDefProd'] = $item['indexdef'];
+//            } else {
+//                if (key_exists($key, $result)){
+//               
+//                        //($result[$key]['indexnameTest'] != $item['indexname'])) {
+//                    $result[$key]['indexnameProd'] = $item['indexname'];
+//                    $result[$key]['indexDefProd'] = $item['indexdef'];
+//                }
+//            }
+//        }
         echo '<pre>';
-        var_dump($result);
+        var_dump($prod);
         echo '</pre>';
         die;
+//        echo '<pre>';
+//        foreach ($onlyTest as $item) {
+//            print('$this->execute("' . $item['indexdef']."\"); \n");
+//        }
+//        echo '</pre>';
+//        die;
         
         
